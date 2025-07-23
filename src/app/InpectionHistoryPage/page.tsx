6fb67fc9014515750a930831248abc115286d3a3
Hafidh <hafidhhusna@mail.ugm.ac.id>
2025-07-23 13:48:15 +0800

    fix folder structure

--- src/app/InpectionHistoryPage/page.tsx
+++ src/app/InpectionHistoryPage/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Dropdown from "../components/Dropdown";
+import Dropdown from "@/components/Dropdown";
 
 const InspectionHistoryPage = () => {
   const handleSubmit = () => {
