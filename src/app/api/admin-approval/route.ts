b4d0fcfe1ea439e4bcc134e602f5a334183a94de
hafidhhusna <hfdhusna@gmail.com>
2025-08-21 14:05:00 +0700

    admin approval

---
+++ src/app/api/admin-approval/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const items = await prisma.item.findMany({
+      include: {
+        nama_lokasi : {select : {nama_lokasi: true}},
+        lokasi_titik_lokasi : {select : {nama_titik_lokasi : true}},
+      },
+      where : {
+        is_deleted : false,
+        status : "PENDING",
+      },
+      orderBy: { id_item: "asc" },
+    });
+
+    console.log("Items dari FindMany : ", items)
+
+    const result = items.map((item) => ({
+      id_item : item.id_item,
+      nama_item : item.nama_item,
+      nomor_seri : item.nomor_ser,
+      nama_lokasi : item.nama_lokasi.nama_lokasi,
+      titik_lokasi : item.lokasi_titik_lokasi.nama_titik_lokasi,
+      jenis_sarana : item.jenis_sarana,
+      spesifikasi : item.spesifikasi,
+      tanggal_pembelian : item.tanggal_pembelian,
+      tanggal_kadaluarsa : item.tanggal_kadaluwarsa,
+      jenis_apap : item.jenis_APAP,
+      berat : item.berat,
+      pemasok : item.pemasok,
+      PIC : item.PIC,
+      status_pemasangan : item.status_pemasangan,
+      uploadedBy : item.uploadedBy
+    }))
+
+    return NextResponse.json({ success: true, items : result});
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
